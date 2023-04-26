7ed5fdde0dca06b700944a55522f51fe7ba504a1
JacobSmith2005 <90261539+JacobSmith2005@users.noreply.github.com>
2023-04-26 14:37:17 -0400

    Updated the app.js
    Updated the app.js to have it include uppy, a drag and drop file uploader for the future.

--- app.js
+++ app.js
@@ -2,12 +2,23 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+var fs = require('fs');
+import Uppy from '@uppy/core';
+import DragDrop from '@uppy/drag-drop';
+import '@uppy/core/dist/style.min.css';
+import '@uppy/drag-drop/dist/style.min.css';
 //part 2, configure expressjs application
 app.set('view engine', 'ejs');
 app.use(bodyparser.urlencoded({
     extended: true
 }))
 app.use(express.static(path.join(__dirname, 'public')));
+
+//fs.readFile('Demo.txt', 'utf8', function(err, data){
+      
+    // Display the file content
+    //console.log(data);
+//});
 
 //part 3 set up endpoints
 app.get('/', (req, res) => {
